ad5b0eea091215bfef787a14eec375958dab0722
Ralf Popescu <ralfpopescu@gmail.com>
2017-01-28 18:39:20 -0500

    connection words

--- smsclientTCP.java
+++ smsclientTCP.java
@@ -1,5 +1,57 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.net.*;
+
 /**
  * Created by ralfpopescu on 1/26/17.
  */
 public class smsclientTCP {
+
+    public static void main(String args[]){
+        String IPAddress = args[0];
+        int portNum = Integer.parseInt(args[1]);
+        ArrayList<String> smsMessage = new ArrayList<String>();
+
+        Scanner sc2 = null;
+        try {
+            sc2 = new Scanner(new File(args[2]));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (sc2.hasNextLine()) {
+            Scanner s2 = new Scanner(sc2.nextLine());
+            while (s2.hasNext()) {
+                String s = s2.next();
+                smsMessage.add(s);
+                System.out.println(s);
+            }
+        }
+
+        Socket MyClient;
+        try {
+            String sentence;
+            String modifiedSentence;
+            //BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+            Socket clientSocket = new Socket(IPAddress, portNum);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            //sentence = inFromUser.readLine();
+
+            String str = "";
+            for(String s:smsMessage){
+                str = str.concat(" " + s);
+            }
+
+
+            outToServer.writeBytes(str);
+            modifiedSentence = inFromServer.readLine();
+            System.out.println("FROM SERVER: " + modifiedSentence);
+            clientSocket.close();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
 }
--- smsengineTCP.java
+++ smsengineTCP.java
@@ -2,6 +2,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
+import java.net.*;
 
 
 /**
@@ -10,8 +11,10 @@ import java.io.*;
 public class smsengineTCP {
 
     public static void main(String args[]){
-        String portNum = args[0];
+        int portNum = Integer.parseInt(args[0]);
         ArrayList<String> susWords = new ArrayList<String>();
+        int spamScore = 0;
+       String susWordsInSMS = "";
 
         Scanner sc2 = null;
         try {
@@ -23,8 +26,40 @@ public class smsengineTCP {
             Scanner s2 = new Scanner(sc2.nextLine());
             while (s2.hasNext()) {
                 String s = s2.next();
+                susWords.add(s);
                 System.out.println(s);
             }
+        }
+
+        try {
+            ServerSocket welcomeSocket = new ServerSocket(portNum);
+            String clientSentence;
+            String capitalizedSentence;
+
+            while(true)
+            {
+                Socket connectionSocket = welcomeSocket.accept();
+                BufferedReader inFromClient =
+                        new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                clientSentence = inFromClient.readLine();
+                System.out.println("Received: " + clientSentence);
+
+                for(String word:susWords){
+                    if(clientSentence.toLowerCase().contains(word.toLowerCase())){
+                        if(!susWords.contains(word)) {
+                            susWordsInSMS = susWordsInSMS + " " + word;
+                        }
+                        spamScore++;
+                    }
+                }
+
+                capitalizedSentence = "" + susWords.size() + " " + spamScore + susWordsInSMS;
+                outToClient.writeBytes(capitalizedSentence);
+                spamScore = 0;
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
         }
     }
 }
